cd884b5e775ce549cd98e2daf4547d425d478865
juanjogomezvillegas <juanjogomvil@gmail.com>
2025-10-22 09:15:41 +0200

    afegir moviments en el tauler

--- index.js
+++ index.js
@@ -20,15 +20,17 @@ class Player {
 
 let player1;
 let player2;
-let board = [];
-for (i = 0; i < 7; i++) {
-    board[i] = new Array(7);
-    for (j = 0; j < 7; j++) {
-        board[i][j] = 0;
-    }
-}
+let board;
 
 window.onload = function() {    
+    board = [];
+    for (i = 0; i < 7; i++) {
+        board[i] = new Array(7);
+        for (j = 0; j < 7; j++) {
+            board[i][j] = 0;
+        }
+    }
+    
     document.title = "Connect Four";
 
     document.getElementById("btnPlay").addEventListener("click", init);
@@ -46,9 +48,15 @@ function init() {
 
         document.getElementById("subtitle").innerHTML = "Score: "+player1.meNameIs() + " o - o " + player2.meNameIs();
 
-        document.getElementById("middle").innerHTML = "";
-        document.getElementById("middle").appendChild(buildBoard(board));
+        insertBoard();
     }
+}
+
+function insertBoard() {
+    let middle = document.getElementById("middle");
+    
+    middle.innerHTML = "";
+    middle.appendChild(buildBoard(board));
 }
 
 function buildBoard(board) {
@@ -57,11 +65,12 @@ function buildBoard(board) {
     let prerow = document.createElement("tr");
     
     for (i = 0; i < 7; i++) {
-        let cell = document.createElement("td");
+        let cell = document.createElement("th");
 
         let button = document.createElement("button");
         button.innerHTML = "V";
         button.setAttribute("class", "btnsBoard");
+        button.setAttribute("onClick", `move(${i})`);
         cell.appendChild(button);
 
         prerow.appendChild(cell);        
@@ -84,4 +93,18 @@ function buildBoard(board) {
     }
 
     return table;
+}
+
+function move(col) {
+    let i = board.length-1;
+    let fi = 0;
+    while (i >= 0 && fi == 0) {
+        if (board[i][col] == 0) {
+            board[i][col] = 1;
+            fi = 1;
+        }
+        i--;
+    }
+    
+    insertBoard();
 }
